a6759668574df66a228be8c278e352f2a0669f02
李宝 <libao@libaos-MacBook-Pro.local>
2017-05-19 16:02:37 +0800

    add theme

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -1,6 +1,11 @@
 import xadmin
-
+from xadmin import views
 from.models import EmailVerifyRecord, Banner
+
+
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
 
 
 class EmailVerifyRecordAdmin(object):
@@ -17,3 +22,4 @@ class BannerAdmin(object):
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
